bc9a4b320ed6d2804fa2457e55e29b7940f9cf95
alaaet <alaaet@gmail.com>
2019-11-03 13:30:12 +0100

    added whitenoise to the middlewares list 2nd try

--- todocrud/settings.py
+++ todocrud/settings.py
@@ -152,5 +152,5 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_DIRS=(os.path.join(BASE_DIR, 'frontend/static/frontend'),)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'todocrud.storage_configs.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
